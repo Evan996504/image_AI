39020a92bbb8ebb838cb8dff8125fab2319081e7
Evan996504 <126270127+Evan996504@users.noreply.github.com>
2023-03-11 18:28:38 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='image_AI',
+    version='0.0.1',
+    author='Qiu',
+    author_email='asd66448@126.com',
+    description='适用于图片提取',
+    long_description_content_type="""text/markdown""",
+    url='你的网站',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
